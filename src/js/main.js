13c68b4e5e999cbccf4e93b173e5abb8429aa575
ChathamHung <hungchatham@gmail.com>
2025-06-27 20:24:51 +0100

    update 3: Screen size and Menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+const title = document.querySelector('.title');
 const menuBtn = document.querySelector('.menu-button');
 const menuPanel = document.querySelector('.menu');
 
@@ -6,11 +7,25 @@ menuPanel.classList.add('menu-hidden');
 
 menuBtn.addEventListener('click', () => {
   menuPanel.classList.toggle('menu-hidden');
+  menuBtn.setAttribute('data-tooltip', menuPanel.classList.contains('menu-hidden')
+    ? 'Menu'
+    : 'Close Menu');
+  
+  menuBtn.querySelector('img').src = menuPanel.classList.contains('menu-hidden')
+    ? './res/images/icons/menu-icon.png'
+    : './res/images/icons/close-icon.png';
 });
+
+title.addEventListener('click', () => {
+  document.location.href = 'index.html';
+});
+
+
 
 // Optional: close menu when clicking outside
 document.addEventListener('click', (e) => {
   if (!menuPanel.contains(e.target) && !menuBtn.contains(e.target)) {
     menuPanel.classList.add('menu-hidden');
+    menuBtn.querySelector('img').src = './res/images/icons/menu-icon.png';
   }
 });
